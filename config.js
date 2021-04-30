3bb699d986cbd182ace7a1d3edec49c34d6533c8
naciom9 <naciom9@yahoo.com>
2021-04-29 23:54:26 -0500

    OAuth with passport and Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '206473371042944',
+        clientSecret: 'f09fccf29a140e69f118bb2b0634e64e'
+    }
 }
--- models/user.js
+++ models/user.js
@@ -11,6 +11,7 @@ const userSchema = new Schema({
     type: String,
     default: ''
   },
+  facebook: String,
   admin: {
     type: Boolean,
     default: false
